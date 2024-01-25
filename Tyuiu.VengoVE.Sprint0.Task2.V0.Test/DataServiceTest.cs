771f1ffcdd68dcd9b80a095a2d8d653fb7f5279b
VengoVE <v726562@gmail.com>
2024-01-25 10:55:48 +0300

    Task2

---
+++ Tyuiu.VengoVE.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.VengoVE.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.VengoVE.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var name = "Валентин";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, Валентин", res);
+        }
+    }
+}
